d905a1873ad9eb9af24f2cd449b7f56b6cca6ea5
rejo132 <enoch.luke1@student.moringaschool.com>
2025-03-22 12:19:27 +0300

    competes lab

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// index.js
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
